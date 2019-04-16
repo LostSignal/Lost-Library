62372420fde47c400390f0e695005c6816d21568
Brian Gish <gish.brian@gmail.com>
2019-04-16 14:29:40 -0700

    Removing comments from editorconfig, adding DisableAnimatorOnExit script.

---
+++ Lost/Lost.Behaviours/Runtime/Statemachine/DisableAnimatorOnExit.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DisableAnimatorOnExit.cs" company="Lost Signal LLC">
+//     Copyright (c) Lost Signal LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Hourglass
+{
+    using UnityEngine;
+
+    public class DisableAnimatorOnExit : StateMachineBehaviour
+    {
+        public override void OnStateMachineExit(Animator animator, int stateMachinePathHash) 
+        {
+            animator.enabled = false;
+        }
+    }
+}
