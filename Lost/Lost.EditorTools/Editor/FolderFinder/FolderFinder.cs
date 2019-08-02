0dbeb10641a00d029e6299fd42866f701cf5b003
Brian Gish <gish.brian@gmail.com>
2019-08-02 00:56:00 -0700

    Adding tool to help find unity special folders

--- Lost/Lost.EditorTools/Editor/FolderFinder/FolderFinder.cs
+++ Lost/Lost.EditorTools/Editor/FolderFinder/FolderFinder.cs
@@ -7,25 +7,49 @@
 namespace Lost
 {
     using System;
+    using System.IO;
     using UnityEditor;
+    using UnityEngine;
 
     public static class FolderFinder
     {
-        //// [MenuItem("Lost/Logs/Show Editor Logs")]
-        //// public static void OpenEditorLogs()
-        //// {
-        ////     // macOS    ~/Library/Logs/Unity/Editor.log
-        ////     // Windows  C:\Users\username\AppData\Local\Unity\Editor\Editor.log
-        ////     EditorUtility.RevealInFinder(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
-        //// }
-        ////
-        //// [MenuItem("Lost/Logs/Show Player Log")]
-        //// public static void OpenPlayerLog()
-        //// {
-        ////     // macOS    ~/Library/Logs/Unity/Player.log
-        ////     // Windows  C:\Users\username\AppData\LocalLow\CompanyName\ProductName\Player.log
-        ////     // Linux    ~/.config/unity3d/CompanyName/ProductName/Player.log
-        ////     EditorUtility.RevealInFinder(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
-        //// }
+        [MenuItem("Lost/Logs/Show Editor Logs")]
+        public static void OpenEditorLogs()
+        {
+            if (Platform.EditorPlatform == Platform.UnityEditorPlatform.Windows)
+            {
+                EditorUtility.RevealInFinder(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Unity", "Editor", "Editor.log"));
+            }
+            else if (Platform.EditorPlatform == Platform.UnityEditorPlatform.Mac)
+            {
+                EditorUtility.RevealInFinder("~/Library/Logs/Unity/Editor.log");
+            }
+            else
+            {
+                Debug.LogError("Unable to open Editor Log...  Unknown Platform.");
+            }
+        }
+
+        [MenuItem("Lost/Logs/Show Player Log")]
+        public static void OpenPlayerLog()
+        {
+            if (Platform.EditorPlatform == Platform.UnityEditorPlatform.Windows)
+            {
+                string appDataRootPath = Path.GetDirectoryName(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)));
+                EditorUtility.RevealInFinder(Path.Combine(appDataRootPath, "LocalLow", PlayerSettings.companyName, PlayerSettings.productName, "Player.log"));
+            }
+            else if (Platform.EditorPlatform == Platform.UnityEditorPlatform.Mac)
+            {
+                EditorUtility.RevealInFinder("~/Library/Logs/Unity/Player.log");
+            }
+            else if (Platform.EditorPlatform == Platform.UnityEditorPlatform.Linux)
+            {
+                EditorUtility.RevealInFinder(Path.Combine("~", ".config", "unity3d", PlayerSettings.companyName, PlayerSettings.productName, "Player.log"));
+            }
+            else
+            {
+                Debug.LogError("Unable to open Editor Log...  Unknown Platform.");
+            }
+        }
     }
 }
