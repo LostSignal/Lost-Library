0d6b796165a23f0d2317297f7fabaa6ca31aec1c
Brian Gish <gish.brian@gmail.com>
2019-08-02 00:55:29 -0700

    Adding helper behaviour to deisbale objects over time

---
+++ Lost/Lost.Behaviours/Runtime/DelayDisableGameObject.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DelayDisableGameObject.cs" company="Lost Signal LLC">
+//     Copyright (c) Lost Signal LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Lost
+{
+    using UnityEngine;
+
+    public class DelayDisableGameObject : MonoBehaviour
+    {
+        #pragma warning disable 0649
+        [SerializeField] private float secondsToWait = 2.0f;
+        #pragma warning restore 0649
+
+        private void OnEnable()
+        {
+            this.ExecuteDelayed(this.secondsToWait, this.DisableGameObject);
+        }
+
+        private void DisableGameObject()
+        {
+            this.gameObject.SafeSetActive(false);
+        }
+    }
+}
