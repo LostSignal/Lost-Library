2ffa8b01a71fbfe057d7570002362601847eb9e1
Brian Gish <gish.brian@gmail.com>
2019-08-02 01:02:22 -0700

    Adding ability to delete/rename sub animations from animators.

--- Lost/Lost.EditorTools/Editor/AnimatorTools/AnimatorEditorWindow.cs
+++ Lost/Lost.EditorTools/Editor/AnimatorTools/AnimatorEditorWindow.cs
@@ -10,6 +10,7 @@ namespace Lost
     using UnityEngine;
     using UnityEditor;
     using UnityEditor.Animations;
+    using UnityEditor.VersionControl;
 
     public class AnimatorEditorWindow : EditorWindow
     {
@@ -18,35 +19,107 @@ namespace Lost
         [MenuItem("Assets/Animator/Add Sub-Animation Clip", true, 100)]
         public static bool AddSubAnimationClipToAnimatorValidator()
         {
-            return Selection.objects.Length == 1 && Selection.objects[0] is AnimatorController;
+            return Selection.objects.Length == 1 && Selection.objects.FirstOrDefault() is AnimatorController;
         }
 
         [MenuItem("Assets/Animator/Add Sub-Animation Clip", false, 100)]
         public static void AddSubAnimationClipToAnimator()
         {
             var window = (AnimatorEditorWindow)EditorWindow.GetWindow(typeof(AnimatorEditorWindow));
+            window.animationClipName = string.Empty;
             window.titleContent = new GUIContent("Add Anim");
             window.minSize = new Vector2(300, 75);
             window.maxSize = new Vector2(300, 75);
             window.ShowPopup();
         }
 
-        private void OnGUI()
+        [MenuItem("Assets/Animator/Rename Sub-Animation Clip", true, 101)]
+        public static bool RenameSubAnimationClipToAnimatorValidator()
         {
-            AnimatorController animator = Selection.objects.FirstOrDefault() as AnimatorController;
+            var animationClip = Selection.objects.FirstOrDefault() as AnimationClip;
+            var animationController = AssetDatabase.LoadAssetAtPath<AnimatorController>(AssetDatabase.GetAssetPath(animationClip));
 
-            if (animator == null)
+            return Selection.objects.Length == 1 && animationClip != null && animationController != null;
+        }
+
+        [MenuItem("Assets/Animator/Rename Sub-Animation Clip", false, 101)]
+        public static void RenameSubAnimationClipToAnimator()
+        {
+            var animationClip = Selection.objects.FirstOrDefault() as AnimationClip;
+
+            var window = (AnimatorEditorWindow)EditorWindow.GetWindow(typeof(AnimatorEditorWindow));
+            window.titleContent = new GUIContent("Rename Anim");
+            window.animationClipName = animationClip.name;
+            window.minSize = new Vector2(300, 75);
+            window.maxSize = new Vector2(300, 75);
+            window.ShowPopup();
+        }
+
+        [MenuItem("Assets/Animator/Delete Sub-Animation Clip", true, 200)]
+        public static bool DeleteSubAnimationClipToAnimatorValidator()
+        {
+            var animationClip = Selection.objects.FirstOrDefault() as AnimationClip;
+            var animationController = AssetDatabase.LoadAssetAtPath<AnimatorController>(AssetDatabase.GetAssetPath(animationClip));
+
+            return Selection.objects.Length == 1 && animationClip != null && animationController != null;
+        }
+
+        [MenuItem("Assets/Animator/Delete Sub-Animation Clip", false, 200)]
+        public static void DeleteSubAnimationClipToAnimator()
+        {
+            var animationClip = Selection.objects.FirstOrDefault() as AnimationClip;
+            var animationController = AssetDatabase.LoadAssetAtPath<AnimatorController>(AssetDatabase.GetAssetPath(animationClip));
+
+            if (Provider.isActive)
             {
-                return;
+                Provider.Checkout(animationController, CheckoutMode.Asset);
             }
 
-            this.animationClipName = EditorGUILayout.TextField("Animation Clip Name", this.animationClipName);
-
-            if (GUILayout.Button("Add"))
+            AssetDatabase.RemoveObjectFromAsset(animationClip);
+            AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(animationController));
+        }
+        
+        private void OnGUI()
+        {
+            if (Selection.objects.FirstOrDefault() is AnimatorController)
             {
-                AssetDatabase.AddObjectToAsset(new AnimationClip() { name = animationClipName }, animator);
-                AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(animator));
-                this.animationClipName = string.Empty;
+                // This is an "Add" operation
+                this.animationClipName = EditorGUILayout.TextField("Animation Clip Name", this.animationClipName);
+
+                if (GUILayout.Button("Add"))
+                {
+                    var animationController = Selection.objects.FirstOrDefault() as AnimatorController;
+
+                    if (Provider.isActive)
+                    {
+                        Provider.Checkout(animationController, CheckoutMode.Asset);
+                    }
+
+                    AssetDatabase.AddObjectToAsset(new AnimationClip() { name = animationClipName }, animationController);
+                    AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(animationController));
+
+                    this.animationClipName = string.Empty;
+                }
+            }
+            else if (Selection.objects.FirstOrDefault() is AnimationClip)
+            {
+                // This is a "Rename" operation
+                this.animationClipName = EditorGUILayout.TextField("Animation Clip Name", this.animationClipName);
+
+                if (GUILayout.Button("Rename"))
+                {
+                    var animationClip = Selection.objects.FirstOrDefault() as AnimationClip;
+                    var animationController = AssetDatabase.LoadAssetAtPath<AnimatorController>(AssetDatabase.GetAssetPath(animationClip));
+
+                    if (Provider.isActive)
+                    {
+                        Provider.Checkout(animationController, CheckoutMode.Asset);
+                    }
+
+                    animationClip.name = this.animationClipName;
+                    AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(animationController));
+                    this.Close();
+                }
             }
         }
     }
